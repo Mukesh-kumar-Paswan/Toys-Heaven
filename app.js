a6e17694fc76677a36822c54278aca7c82ee2431
Mukesh-kumar-Paswan <h.mukesh.paswan@gmail.com>
2025-09-18 23:33:15 +0530

    First listing

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const port = 8080;
 const Mongo_URL = "mongodb://127.0.0.1:27017/ToyHeaven";
+const listing = require("./models/listing.js");
+
 
 main()
     .then(() => {
@@ -23,3 +25,15 @@ app.listen(port , () => {
 app.get("/" , (req , res) => {
     res.send("Welcome to the toys heaven");
 })
+
+app.get("/testListing" , async (req , res) => {
+    let sample = new listing ({
+        title:"No Image",
+        description: "testing of no image",
+        price:1000,
+    });
+
+    await sample.save();
+    console.log("Sample was saved");
+    res.send("success");
+});
